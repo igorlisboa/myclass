552e88a6b911ad51e4b9a5c0dd171b46774ad481
igorLisboa <igorlisboa34@gmail.com>
2017-12-01 18:54:15 -0200

    atualizacao do app

--- implementacao/myclass/src/components/feed/feed.component.ts
+++ implementacao/myclass/src/components/feed/feed.component.ts
@@ -1,18 +1,68 @@
 import {Component, OnInit} from "@angular/core";
-import {GradeHoraria, PersistenciaService} from "../../infra/persistencia/persistencia.service";
+import {GradeHoraria, Mensagens, PersistenciaService} from "../../infra/persistencia/persistencia.service";
 
 @Component({
-  selector:'mc-feed',
+  selector:'oc-feed',
   templateUrl:'./feed.component.html',
   styles:['./feed.component.scss']
 })
 export class FeedComponent implements OnInit{
   public gradeHoraria:Array<GradeHoraria>;
+  public mensagens:Array<Mensagens>;
+  public aulaHoje:GradeHoraria;
+  public diaDescricao: string;
+  public hoje = new Date();
 
   constructor(private persistencia:PersistenciaService){
   }
 
   ngOnInit(){
     this.gradeHoraria = this.persistencia.getGrade();
+    this.mensagens = this.persistencia.getMensagens();
+
+    this.aulaHoje = this.gradeHoraria.filter(x => x.diaSemana == this.hoje.getDay())[0]
+    this.diaDescricao = this.diaSemanaDescricao(this.aulaHoje.diaSemana);
+
+    this.mensagens = this.mensagens.filter(mens =>mens.data > this.hoje).map((mens,index) =>{
+      if(mens.img == undefined){
+        if(index % 2 == 0){
+          mens.img = '../../assets/imgs/estudando2.jpg';
+        }else {
+          mens.img = '../../assets/imgs/estudando.jpg';
+        }
+      }
+      return mens;
+    })
+
   }
+
+
+  diaSemanaDescricao(codDia:number): string{
+    let temp: string = '';
+      switch (codDia){
+        case 0:
+          temp = 'DOMINGO';
+          break;
+        case 1:
+          temp = 'SEGUNDA';
+          break;
+        case 2:
+          temp = 'TERCA';
+          break;
+        case 3:
+          temp = 'QUARTA';
+          break;
+        case 4:
+          temp = 'QUINTA';
+          break;
+        case 5:
+          temp = 'SEXTA';
+          break;
+        case 6:
+          temp = 'SABADO';
+          break;
+      }
+    return temp;
+  }
+
 }
--- implementacao/myclass/src/infra/persistencia/persistencia.service.ts
+++ implementacao/myclass/src/infra/persistencia/persistencia.service.ts
@@ -13,56 +13,82 @@ export class PersistenciaService{
         horario : 1,
         nomeAula : "Proj. Lógico de Computadores",
         prof : "Ricardo Loiola",
-        alerta : false
+        alerta :[]
       },{
         diaSemana : DiaSemanaEnum.SEGUNDA,
         horario : 2,
         nomeAula : "Processamento de Imagem",
         prof : "Gerson",
-        alerta : false
+        alerta :[]
       },{
         diaSemana : DiaSemanaEnum.TERCA,
         horario : 1,
         nomeAula : "Sistemas de Operacionais Abertos",
         prof : "A Definir",
-        alerta : true
+        alerta :[]
       },{
         diaSemana : DiaSemanaEnum.QUARTA,
         horario : 1,
         nomeAula : "Analise Matemática",
         prof : "Solange",
-        alerta : true
+        alerta :[]
       },{
         diaSemana : DiaSemanaEnum.QUARTA,
         horario : 2,
         nomeAula : "Telecomunicações",
         prof : "Carlos Geber",
-        alerta : true
+        alerta :[]
       },{
         diaSemana : DiaSemanaEnum.QUINTA,
         horario : 1,
         nomeAula : "Sistema de Informações Inteligentes",
         prof : "Robson",
-        alerta : false
+        alerta :[]
       },{
         diaSemana : DiaSemanaEnum.QUINTA,
         horario : 2,
         nomeAula : "Administração",
         prof : "Fernando Bonfim",
-        alerta : false,
+        alerta :[],
         quinzenal:true
       },{
         diaSemana : DiaSemanaEnum.SEXTA,
         horario : 1,
         nomeAula : "CC Interdiciplinar",
         prof : "Gerson",
-        alerta : false
+        alerta :[{
+          data:new Date().setFullYear(2017,11,1),
+          nivelAlerta: 1,
+          texto:"Apresentação dos trabalhos."
+        }]
       }
     ];
 
-    // let gradeHoraria:[GradeHoraria]=
-    // return gradeHoraria;
+  }
 
+  getMensagens():Array<Mensagens>{
+    return [
+      {
+        data:new Date().setFullYear(2017,10,29),
+        materia:'Analise Matematica',
+        descricao:'Prova de Analise Matematica \n Entrega do trabalho de Analise Matematica'
+      },
+      {
+        data:new Date().setFullYear(2017,11,1),
+        materia:'CC Interdiciplinar',
+        descricao:'Apresentação do trabalho de Interdiciplinar'
+      },
+      {
+        data:new Date().setFullYear(2017,11,7),
+        materia:'Administração',
+        descricao:`Vista de NP2 \n Prova Substutiva`
+      },
+      {
+        data:new Date().setFullYear(2017,11,29),
+        materia:'ATENÇÃO !!!',
+        descricao:'Não esqueçam de marcar as provas de DP.'
+      }
+    ];
   }
 }
 
@@ -71,6 +97,21 @@ export interface GradeHoraria{
   horario:number;
   nomeAula:string;
   prof:string;
-  alerta:boolean;
+  alerta:Array<Alerta>;
   quinzenal?:boolean;
 }
+
+export interface Alerta{
+  data:any;
+  nivelAlerta:number;
+  materia?:string;
+  texto:string;
+}
+
+export interface Mensagens{
+  data:any;
+  materia:string;
+  descricao:string;
+  img?:string;
+}
+
